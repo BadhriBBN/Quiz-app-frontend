3fde60a06433c627ead095138c4c51f7231bdb46
BathriBBN <bbnbadhri@gmail.com>
2023-10-03 11:56:00 +0530

    final-commit

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -5,48 +5,45 @@ import { updateResult } from '../hooks/setResult';
 
 export default function Questions({ onChecked }) {
   const [checked, setChecked] = useState(undefined);
-  const { trace } = useSelector((state) => state.questions);
-  const result = useSelector((state) => state.result.result);
+  const { trace } = useSelector(state => state.questions);
+  const result = useSelector(state => state.result.result) 
   const [{ isLoading, apiData, serverError }] = useFetchQuestion();
 
   const questions = useSelector(
     (state) => state.questions.queue[state.questions.trace]
   );
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
-  useEffect(() => {
-    if (trace !== undefined) {
-      dispatch(updateResult({ trace, checked }));
-    }
-  }, [trace, checked, dispatch]);
+  useEffect(() => { 
+    dispatch(updateResult({ trace, checked}))
+  },[checked]);
 
   function onSelect(i) {
-    onChecked(i);
-    setChecked(i);
+    onChecked(i)
+    setChecked(i)
+    dispatch(updateResult({ trace, checked}))
   }
 
-  if (isLoading) return <h3 className="text-light">Loading</h3>;
+  if (isLoading) return <h3 className="text-light">isLoading</h3>;
   if (serverError)
     return <h3 className="text-light">{serverError || 'Unknown Error'}</h3>;
   return (
     <div className="questions">
       <h2 className="text-light">{questions?.question}</h2>
-      <ul>
+      <ul key={questions?.id}>
         {questions?.options.map((q, i) => (
           <li key={i}>
             <input
               type="radio"
-              value={i} // Use the value attribute to store the option index
+              value={false}
               name="options"
               id={`q${i}-option`}
-              onChange={() => onSelect(i)}
+              onChange={()=>onSelect(i)}
             />
             <label className="text-primary" htmlFor={`q${i}-option`}>
               {q}
             </label>
-            <div
-              className={`check ${result[trace] === i ? 'checked' : ''}`}
-            ></div>
+            <div className={`check ${result[trace] == i ? 'checked' : ''}`}></div>
           </li>
         ))}
       </ul>
